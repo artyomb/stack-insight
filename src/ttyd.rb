5534a5869e83983ccf81efa3446805ef2c5ce5e0
artem.borodkin@gmail.com <artem.borodkin@gmail.com>
2025-07-21 00:10:57 +0300

    update

--- src/ttyd.rb
+++ src/ttyd.rb
@@ -3,125 +3,133 @@ require 'async/websocket/adapters/rack'
 require 'async/websocket/client'
 require 'async/http/endpoint'
 require 'net/http'
-require 'uri'
 require 'open3'
 
-$cid2thread = {}
-
 class ServerTtyd < Sinatra::Base
+  TTYD_SESSIONS = {}
+  PORT_RANGE = (8000..8010).freeze
+
   helpers do
-    def new_port
-      port = 8000
-      while port <= 8010
-        return port unless $cid2thread.values.any? { |info| info[:port] == port }
-        port += 1
-      end
-      raise 'No available ports in range 8000-9000'
+    def available_port
+      PORT_RANGE.find { |port| !port_in_use?(port) } ||
+        raise('No available ports in range 8000-8010')
     end
 
-    otl_def def start_ttyd(cid)
+    def port_in_use?(port)
+      TTYD_SESSIONS.values.any? { |session| session[:port] == port }
+    end
 
-      $cid2thread.delete_if do |cid, info|
-        !system("ps ax | grep ttyd | grep #{info[:port]}")
+    def shell_for_container(cid)
+      system("docker exec #{cid} ls /bin/bash") ? 'bash' : 'sh'
+    end
+
+    def cleanup_dead_sessions
+      TTYD_SESSIONS.delete_if do |_, session|
+        !session[:thread]&.alive? || !process_running?(session[:port])
       end
+    end
 
-      ttyd = $cid2thread[cid]
-      return ttyd unless ttyd.nil? || ttyd[:thread].nil?
+    def process_running?(port)
+      system("pgrep -f 'ttyd.*#{port}'", out: File::NULL, err: File::NULL)
+    end
 
-      thrd = Thread.new do
-        path = 'sh'
-        path = 'bash' if system("docker exec #{cid} ls /bin/bash")
+    def start_ttyd_session(cid)
+      cleanup_dead_sessions
 
-        port = new_port
-        logs = []
-        otl_span "ttyd cid: #{cid}, port: #{port}, path: #{path}", { cid: , port:, path: } do
-          $cid2thread[cid] = { thread: thrd, cid:, port:, logs: }
-          Open3.popen3("ttyd -p #{port} -W docker exec -it #{cid} #{path}") do |stdin, stdout, stderr, wait_thr|
-            # Open3.popen3("ttyd -p #{port} -W ssh swarm") do |stdin, stdout, stderr, wait_thr|
-            $cid2thread[cid][:wait_thr] = wait_thr
-            stdin.close
-            while line = stdout.gets; logs << line end
-            while line = stderr.gets; logs << line end
-            wait_thr.join
-            return_value = wait_thr.value
+      return TTYD_SESSIONS[cid] if TTYD_SESSIONS[cid]&.dig(:thread)&.alive?
 
-            $cid2thread[cid][:exit_code] = return_value.exitstatus
-          end
-          puts "ttyd process for container #{cid} exited with code #{$cid2thread[cid][:exit_code]}"
-        end
-      ensure
-        if $cid2thread[:cid]
-          Process.kill("KILL",$cid2thread[:cid][:wait_thr].pid)
-          $cid2thread[:cid][:tread] = nil
-          $cid2thread[:cid][:wait_thr] = nil
-          $cid2thread.delete(cid)
-        end
+      port = available_port
+      shell = shell_for_container(cid)
+
+      thread = Thread.new { run_ttyd_process(cid, port, shell) }
+
+      session = { thread:, port:, cid: }
+      TTYD_SESSIONS[cid] = session
+
+      sleep 0.5 # Allow ttyd to start
+
+      session[:thread].alive? ? session :
+        (TTYD_SESSIONS.delete(cid); raise("Failed to start ttyd for #{cid}"))
+    end
+
+    private
+
+    def run_ttyd_process(cid, port, shell)
+      cmd = "ttyd -p #{port} -W docker exec -it #{cid} #{shell}"
+
+      Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
+        stdin.close
+
+        # Log output in background
+        Thread.new { stdout.each_line { |line| puts "[ttyd-#{cid}] #{line}" } }
+        Thread.new { stderr.each_line { |line| puts "[ttyd-#{cid}] ERROR: #{line}" } }
+
+        wait_thr.join
+        puts "ttyd process for #{cid} exited with code #{wait_thr.value.exitstatus}"
       end
-
-      sleep 1
-      if $cid2thread[cid][:thread].nil?
-        $cid2thread.delete(cid)
-        raise "ttyd process for container #{cid} failed to start"
-      end
-      $cid2thread[cid]
+    ensure
+      TTYD_SESSIONS.delete(cid)
     end
   end
 
   get '/ttyd/:cid/ws' do
-    ttyd = start_ttyd params[:cid]
+    session = start_ttyd_session(params[:cid])
 
-    if Async::WebSocket::Adapters::Rack.websocket?(env)
-      protocols = env['HTTP_SEC_WEBSOCKET_PROTOCOL']&.split(',')&.map(&:strip) || []
-      selected_protocol = protocols.include?('tty') ? 'tty' : nil
+    return unless Async::WebSocket::Adapters::Rack.websocket?(env)
 
-      Async::WebSocket::Adapters::Rack.open(env, protocols: selected_protocol ? [selected_protocol] : []) do |client|
-        endpoint = Async::HTTP::Endpoint.parse("ws://localhost:#{ttyd[:port]}/ws")
+    protocols = env['HTTP_SEC_WEBSOCKET_PROTOCOL']&.split(',')&.map(&:strip) || []
+    selected = protocols.include?('tty') ? ['tty'] : []
 
-        Async::WebSocket::Client.connect(endpoint, protocols: ['tty']) do |ttyd_con|
-
-          ttyd_to_client = Async do
-            while message = ttyd_con.read
-              client.write(message)
-              client.flush
-            end
-          ensure
-            client.close rescue nil
-          end
-
-          client_to_ttyd = Async do
-            while message = client.read
-              ttyd_con.write(message)
-              ttyd_con.flush
-            end
-          ensure
-            ttyd_con.close rescue nil
-          end
-
-          ttyd_to_client.wait
-          client_to_ttyd.wait
-        rescue => e
-          puts "ws client error: #{e.message}"
-        ensure
-          Process.kill("KILL",ttyd[:wait_thr].pid)
-          # ttyd[:thread]&.exit rescue nil
-          ttyd[:thread] = nil
-          $cid2thread.delete(params[:cid])
-        end
-
-      rescue => e
-        puts "ws adapter error: #{e.message}"
-        client.close rescue nil
-      end
+    Async::WebSocket::Adapters::Rack.open(env, protocols: selected) do |client|
+      proxy_websocket_connection(client, session[:port])
     end
-  rescue =>e
-    puts "get ws error: #{e.message}"
+  rescue => e
+    puts "WebSocket error for #{params[:cid]}: #{e.message}"
   end
 
   get '/ttyd/:cid*' do
-    ttyd = start_ttyd params[:cid]
-
+    session = start_ttyd_session(params[:cid])
     path = params['splat'].first.split('/').last
-    uri = URI("http://localhost:#{ttyd[:port]}/#{path}")
+
+    proxy_http_request(session[:port], path)
+  rescue => e
+    puts "HTTP proxy error for #{params[:cid]}: #{e.message}"
+    halt 500, "Internal server error"
+  end
+
+  private
+
+  def proxy_websocket_connection(client, port)
+    endpoint = Async::HTTP::Endpoint.parse("ws://localhost:#{port}/ws")
+
+    Async::WebSocket::Client.connect(endpoint, protocols: ['tty']) do |ttyd_ws|
+      # Bidirectional message proxying
+      tasks = [
+        Async { proxy_messages(ttyd_ws, client) },
+        Async { proxy_messages(client, ttyd_ws) }
+      ]
+
+      tasks.each(&:wait)
+    end
+  rescue => e
+    puts "WebSocket proxy error: #{e.message}"
+  ensure
+    client.close rescue nil
+  end
+
+  def proxy_messages(from, to)
+    while message = from.read
+      to.write(message)
+      to.flush
+    end
+  rescue => e
+    puts "Message proxy error: #{e.message}"
+  ensure
+    to.close rescue nil
+  end
+
+  def proxy_http_request(port, path)
+    uri = URI("http://localhost:#{port}/#{path}")
     response = Net::HTTP.get_response(uri)
 
     content_type response['content-type'] if response['content-type']
